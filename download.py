b5fdecf104c4d447a50cf25d09869e41c7697ae7
junyoung-sim <jysim.ny@gmail.com>
2023-08-20 16:53:27 -0400

    2023-08-20

---
+++ download.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import certifi
+import warnings
+import json, csv
+import pandas as pd
+from urllib.request import urlopen
+
+warnings.filterwarnings("ignore")
+
+def get_jsonparsed_data(url):
+    response = urlopen(url, cafile=certifi.where())
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+def download(ticker, apikey):
+    url = "https://financialmodelingprep.com/api/v3/historical-price-full"
+    if ticker.endswith("=X"):
+        url += "/{}?apikey={}&from=2000-01-01" .format("USD" + ticker[:-2], apikey)
+    else:
+        url += "/{}?apikey={}&from=2000-01-01" .format(ticker, apikey)
+
+    json_data = get_jsonparsed_data(url)
+    out = open("./data/{}.csv" .format(ticker), "w")
+    csv_writer = csv.writer(out)
+
+    header = True
+    for data in json_data["historical"]:
+        if header:
+            csv_writer.writerow(data.keys())
+            header = False
+        csv_writer.writerow(data.values())
+    out.close()
+
+    dat = pd.read_csv("./data/{}.csv" .format(ticker)).loc[::-1][["date", "adjClose"]].reset_index().drop(columns=["index"])
+    return dat
